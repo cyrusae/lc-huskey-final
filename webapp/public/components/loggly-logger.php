3ea6ca9a1e1db425716182315bcec5233a2f593e
Cyrus <86699936+cyrusae@users.noreply.github.com>
2024-08-14 15:37:03 -0700

    qol: reference info

--- webapp/public/components/loggly-logger.php
+++ webapp/public/components/loggly-logger.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\LogglyHandler;
 use Monolog\Formatter\LogglyFormatter;
-
+// Details about the Loggly Monolog format: https://github.com/Seldaek/monolog/blob/main/src/Monolog/Formatter/LogglyFormatter.php
 $logglyToken = $_ENV["LOGGLY_TOKEN"];
 
 $logger = new Logger('UW HusKey Manager');
